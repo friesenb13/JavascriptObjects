cb8f98e4fd94faa2a13b4a61552bc3778a47a146
Becky <77301055+friesenb13@users.noreply.github.com>
2021-03-01 15:27:38 -0700

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,53 @@
 console.log("connected");
 
-// var quote1 = {
-// 	quote: '"We don\'t make mistakes, just happy little accidents."-Bob Ross',
-// 	author: "Bob Ross",
-// 	tags: ["painting", "mistakes"],
-// 	color: "#0a3410",
-// 	display: function() {
-// 		var quoteString = "";
-// 		quoteString += "<div style='background:" + this.color + "'>";
-// 		quoteString += "<p>" + this.quote + "</p>";
-// 		quoteString += "<cite>" + this.author + "</cite";
-// 		quoteString += "</div>"
-// 		$("body").prepend(quoteString)
-// 	}
-// }
+var quote1 = {
+	quote: '"We don\'t make mistakes, just happy little accidents."-Bob Ross',
+	author: "Bob Ross",
+	tags: ["painting", "mistakes"],
+	color: "#0a3410",
+	display: function() {
+		var quoteString = "";
+		quoteString += "<div style='background:" + this.color + "'>";
+		quoteString += "<p>" + this.quote + "</p>";
+		quoteString += "<cite>" + this.author + "</cite";
+		quoteString += "</div>"
+		$("body").prepend(quoteString)
+	}
+}
+var quote2 = {
+	quote: '"Creativity takes courage."- Henry Matisse',
+	author: "Henry Matisse",
+	tags: ["courage", "Creativity"],
+	color: "#154175",
+	display: function() {
+		var quoteString = "";
+		quoteString += "<div style='background:" + this.color + "'>";
+		quoteString += "<p>" + this.quote + "</p>";
+		quoteString += "<cite>" + this.author + "</cite";
+		quoteString += "</div>"
+		$("body").prepend(quoteString)
+	}
+}
+var quote3 = {
+	quote: '"There\s nothing wrong with having a tree as a friend"-Bob Ross',
+	author: "Bob Ross",
+	tags: ["painting", "mistakes"],
+	color: "#6E1DA5",
+	display: function() {
+		var quoteString = "";
+		quoteString += "<div style='background:" + this.color + "'>";
+		quoteString += "<p>" + this.quote + "</p>";
+		quoteString += "<cite>" + this.author + "</cite";
+		quoteString += "</div>"
+		$("body").prepend(quoteString)
+	}
+}
 
 
 
-// quote1.display();
+quote1.display();
+quote2.display();
+quote3.display();
 
 
 
